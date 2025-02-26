88affa8bfbd9214c54129958ce043b67c8daea63
modularProyect2024 <modular.server2024@gmail.com>
2025-02-25 20:14:42 -0600

    Add files via upload

---
+++ mi-proyecto/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>Bienvenido a la mejor app de Fútbol Mexicano ⚽</p>
+    <ul>
+      <li v-for="equipo in equipos" :key="equipo.id">{{ equipo.nombre }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, inject } from 'vue';
+
+const title = inject('title');
+const equipos = ref([
+  { id: 1, nombre: 'América' },
+  { id: 2, nombre: 'Chivas' },
+  { id: 3, nombre: 'Cruz Azul' },
+  { id: 4, nombre: 'Pumas' }
+]);
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  font-size: 18px;
+  margin: 5px 0;
+}
+</style>
